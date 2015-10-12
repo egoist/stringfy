268f1e8738029737df8e03a5526f32c059e2af08
egoist <aprilorange.net@icloud.com>
2015-10-12 15:42:42 +0800

    upadte test

--- test.js
+++ test.js
@@ -5,13 +5,21 @@ var should = require('should')
 describe('Stringfy', () => {
   describe('camelCase', () => {
 
-    it('should return camelCased string getElementById', () => {
+    it('should return camelCased string getElementById from -', () => {
       stringfy('get-element-by-id').camelCase().val().should.equal('getElementById')
     })
+    it('should return camelCased string getElementById from _', () => {
+      stringfy('get_element_by_id').camelCase().val().should.equal('getElementById')
+    })
+    it('should return camelCased string getElementById from both _ and -', () => {
+      stringfy('get_element_by-id').camelCase().val().should.equal('getElementById')
+    })
 
+  })
+
+  describe('reverse', () => {
     it('should return reversed string edcba', () => {
       stringfy('abcde').reverse().val().should.equal('edcba')
     })
-
   })
 })
